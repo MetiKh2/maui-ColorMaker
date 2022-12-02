86fdd9167c4c1c74e1208b48c4e468e7ddabd366
meti <mahdikhodarahimi0@gmail.com>
2022-12-02 18:17:33 +0330

    Add project files.

---
+++ ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace ColorMaker;
+using Microsoft.Maui.Graphics;
+using System.Drawing;
+
+public partial class MainPage : ContentPage
+{
+    public short Red { get; set; }
+    public short Blue { get; set; }
+    public short Green{ get; set; }
+    public string HEX { get; set; }
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void sliderBlue_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Blue = (short)sliderBlue.Value;
+        GenerateRGB();
+    }
+
+    private void sliderRed_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Red= (short)sliderRed.Value;
+        GenerateRGB();
+
+    }
+
+    private void sliderGreen_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Green = (short)sliderGreen.Value;
+        GenerateRGB();
+    }
+    private void GenerateRGB()
+    {
+        System.Drawing.Color myColor = System.Drawing.Color.FromArgb(Red, Green, Blue);
+        lbl_hex.Text = "#"+myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+        Container.BackgroundColor= Microsoft.Maui.Graphics.Color.FromRgb(Red,Green,Blue);
+    }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+        Random random = new ();
+        Blue = (short)random.NextInt64(0,256);
+        sliderBlue.Value = Blue;
+        Green= (short)random.NextInt64(0,256);
+        sliderGreen.Value = Green;
+        Red = (short)random.NextInt64(0,256);
+        sliderRed.Value = Red;
+        GenerateRGB();
+    }
+
+    private async void btnCopy_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.Default.SetTextAsync(lbl_hex.Text);
+    }
+}
+
